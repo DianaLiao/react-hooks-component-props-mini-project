ba66825b64e82581ebd70b0535c57a245c57becc
Diana Liao <dijali@gmail.com>
2021-04-27 15:42:34 -0400

    Done.

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import Article from "./Article"
+
+function ArticleList({posts}){
+  return(
+    <main>
+      {posts.map(post => <Article key={post.id} {...post}/>)}
+    </main>
+  )
+
+}
+
+export default ArticleList
